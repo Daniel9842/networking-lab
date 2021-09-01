d418c17f041e26d5a665f74b029b7570c5d4ac39
Daniel Ducuara <daniel.ducuara@mail.escuelaing.edu.co>
2021-09-01 15:42:44 -0500

    first commit

---
+++ src/test/java/edu/escuelaing/arsw/Networking/NetworkingTest.java
@@ -0,0 +1,15 @@
+package edu.escuelaing.arsw.Networking;
+
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for networking.
+ */
+public class NetworkingTest 
+    extends TestCase
+{
+	
+}
